6c7a9817ffd456dbc460b0406034971e64c97cda
Stephania Mwikali <steph.mwikali@gmail.com>
2019-08-01 15:14:24 +0300

    This commit will add functionality to the forms

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+//Business logic
+function Styles(style, salonist) {
+  this.style = style;
+  this.salonist = salonist;
+}
+Styles.prototype.totalCost = function() {
+  return this.style + this.salonist;
+}
+
+//User interface logic
 $(document).ready(function() {
   $("#img4").on("click", function() {
     $("#detail4").toggle('slow');
@@ -29,3 +39,45 @@ $(document).ready(function() {
   $("#radio61").on("click", function() {
     $("#myForm6").hide('slow');
   });
+  $("form#myForm4").submit(function(event) {
+    event.preventDefault();
+    var mwamba = $("input[name='salonist']:checked", '#forForm').val();
+    var hairStyle = parseInt($(this).find($("select#valueSelect3")).val());
+    // alert(hairStyle);
+    var radio = $("input[name='salonist']:checked").val();
+    if (radio == "yes") {}
+    if (radio == "yes") {
+      var rosette = parseInt(1500);
+    }
+    var totalPayment = new Styles(rosette, hairStyle);
+    // alert("Your total cost is Ksh. " + totalPayment.totalCost());
+    $("p#mwamcost").append("Cost of hairstyle is Ksh. " + hairStyle + " " + "and " + "cost of service is Ksh. " + rosette + ". " + "Total cost is Ksh. " + totalPayment.totalCost()) + ".";
+  });
+  $("form#myForm5").submit(function(event) {
+    event.preventDefault();
+    var mwamba = $("input[name='salonist']:checked", '#forForm').val();
+    var hairStyle = parseInt($(this).find($("select#valueSelect4")).val());
+    // alert(hairStyle);
+    var radio = $("input[name='salonist']:checked").val();
+    if (radio == "yes") {}
+    if (radio == "yes") {
+      var samson = parseInt(1000);
+    }
+
+    var totalPayment = new Styles(samson, hairStyle);
+    $("p#kabcost").append("Cost of hairstyle is Ksh. " + hairStyle + " " + "and " + "cost of service is Ksh. " + samson + ". " + "Total cost is Ksh. " + totalPayment.totalCost()) + ".";
+  });
+  $("form#myForm6").submit(function(event) {
+    event.preventDefault();
+    var mweheni = $("input[name='salonist']:checked", '#forForm').val();
+    var hairStyle = parseInt($(this).find($("select#valueSelect5")).val());
+    // alert(hairStyle);
+    var radio = $("input[name='salonist']:checked").val();
+    if (radio == "yes") {}
+    if (radio == "yes") {
+      var shantel = parseInt(900);
+    }
+    var totalPayment = new Styles(shantel, hairStyle);
+    $("p#mwecost").append("Cost of hairstyle is Ksh. " + hairStyle + " " + "and " + "cost of service is Ksh. " + shantel + ". " + "Total cost is Ksh. " + totalPayment.totalCost()) + ".";
+  });
+});
